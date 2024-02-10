cb99645076de482d27206c8058b2bcd66f16960f
Aknight08 <ameykohale47@gmail.com>
2024-02-10 12:22:41 +0530

    9th feb updates

--- demo/src/main/java/com/simulator/controller/ProcessSimulatorController.java
+++ demo/src/main/java/com/simulator/controller/ProcessSimulatorController.java
@@ -1,5 +1,38 @@
 package com.simulator.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.simulator.service.SchedulerService;
+
+@RestController
+@RequestMapping(value="/api", method= RequestMethod.POST )
 public class ProcessSimulatorController {
+	
+	private final SchedulerService schedulerService;
+	//@PostMapping(value="")
+	public ProcessSimulatorController(SchedulerService schedulerService) {
+		super();
+		this.schedulerService = schedulerService;
+		
+	}
+
+
+	@PostMapping(value="/simulator")
+	public scheduleProcesses(@RequestBody ScheduleRequest  request,@RequestParam String algorithm)
+	List<Process> processes =request.getProcesses();
+	
+	public ResponseEntity<List<Process>> scheduleProcess(@RequestBody List<Process> processes,@RequestParam String algorithm) {
+		List<Process>scheduleProcesses = SchedulerService.scheduleProcesses(,algorithm);
+		return ResponseEntity.ok(scheduleProcesses);
+		}
+	}
 
 }
